b1b629e3ccf1ade4a597f27aee6a22f34f0a9c19
Oscar Stommendal <oscar.stommendal01@gmail.com>
2024-11-21 00:24:07 +0100

    continued the work on H1, code for task 2 and 3 seems to work fine now

--- H1/src/run.c
+++ H1/src/run.c
@@ -11,11 +11,9 @@
 void calculate(double *potential, double *virial, double **force,
            double **position, double cell_length, int nbr_atoms);
 gsl_rng *init_gsl_rng(int seed);
-void verlet(double **positions, double **velocities, double **forces, int its, 
-            double a, double delta_t, double m, double k_B, int N, FILE *fp);
-void equilibrium(int its, int N, double T_eq, double P_eq, double **positions, 
-            double **velocities, double *T, double *P, double *a, double a_0, double m,
-            double beta, double k_B, double delta_t, FILE *fp);
+void verlet(double **positions, double **velocities, double **forces, int its, int its_eq,
+            double delta_t, double m, double k_B, double a_0, double beta, int N, 
+            FILE *fp, double T_eq, double P_eq);
 
 int
 run(
@@ -31,6 +29,7 @@ run(
     int N = 256; // Number of atoms
     double k_B = 8.617333262145 * 1e-5; // Boltzmann constant [eV/K]
     double m = 27.0 / 9649; // Aluminium mass [eV ps²/Å²]
+    double beta = 1. / (76e9 * 6.2415103e-12); // In [Å³/eV]
 
     // ********************************** Task 1 ********************************** //
 
@@ -59,30 +58,32 @@ run(
 
     // ********************************* Task 2 ********************************** //
 
-    int t_max = 5;
+    int t_max = 10;
     const double delta_t = atof(argv[1]);
     int its = (int)(t_max / delta_t);
+    int its_eq = 0;
 
     double a_0 = 4.0;
     double **positions = create_2D_array(N, 3);
     double **forces = create_2D_array(N, 3);
     double **velocities = create_2D_array(N, 3);
     for (int i = 0; i < N; i++)
-    {
-        for (int j = 0; j < 3; j++)
         {
-            velocities[i][j] = 0.0;
+            for (int j = 0; j < 3; j++)
+            {
+                velocities[i][j] = 0.0;
+            }
         }
-    }
     init_fcc(positions, 4, a_0);
     // Initialize the random number generator
     gsl_rng *r = init_gsl_rng(42);
 
-    // char filename[50];
+    char filename[50];
     // Format the filename with delta_t
-    // sprintf(filename, "data/task_2/data_dt_%.3f_%i.csv", delta_t, its);
-    // FILE *fp = fopen(filename, "w");
-    // fprintf(fp, "%i, %i, %f, %f\n", its, t_max, delta_t, 0.);
+    sprintf(filename, "data/task_2/data_dt_%.3f_%i_%i.csv", delta_t, its, its_eq);
+    FILE *fp = fopen(filename, "w");
+    fprintf(fp, "its, t_max, delta_t, its_eq\n%i, %i, %f, %i\n", its, t_max, delta_t, its_eq);
+    fprintf(fp, "E_kin, E_pot, E_tot, <T>\n");
 
     for (int i = 0; i < N; i++)
         {   
@@ -90,53 +91,27 @@ run(
             addition_with_constant(positions[i], positions[i], a_0 * (1 - rng), 3);
         }
 
-    verlet(positions, velocities, forces, its, a_0, delta_t, m, k_B, N, NULL);
-
-    // fclose(fp);
-    gsl_rng_free(r);
-    // destroy_2D_array(positions);
-    destroy_2D_array(forces);
-    // destroy_2D_array(velocities);
+    verlet(positions, velocities, forces, its, its_eq, delta_t, m, k_B, beta, a_0, N, fp, 0., 0.);
 
     // ********************************* Task 3 ********************************** //
 
-    double beta = 1. / (76e9 * 6.2415103e-12); // In [Å³/eV]
-    double T_eq = 500.;
-    double P_eq = 0.1e6 * 6.2415103e-12; // In [eV/Å³]
-    int its_new = 5000;
-    // double t_max = 10;
-    // double a_0 = 4.0;
-
-    // double delta_t = 0.001;
-    // int its = (int)(t_max / delta_t);
-    FILE *fp_pos = fopen("data/task_3/positions.xyz", "w");
-    write_xyz(fp_pos, "Al", positions, velocities, a_0 * N, N);
+    // double T_eq = 500.;
+    // double P_eq = 0.1e6 * 6.2415103e-12; // In [eV/Å³]
 
     // Format the filename with delta_t
-    char filename[50];
-    sprintf(filename, "data/task_3/data_dt_%.3f_%i.csv", delta_t, its);
-    FILE *fp = fopen(filename, "w");
-
-    // double **positions = create_2D_array(N, 3);
-    // double **velocities = create_2D_array(N, 3);
-    double *T = malloc(its_new * sizeof(double));
-    double *P = malloc(its_new * sizeof(double));
-    double *a = malloc(its_new * sizeof(double));
-    // for (int i = 0; i < N; i++)
-    // {
-        // for (int j = 0; j < 3; j++)
-        // {
-        //     velocities[i][j] = 0.0;
-        // }
-    // }
-    // init_fcc(positions, 4, a_0);
-    equilibrium(its_new, N, T_eq, P_eq, positions, velocities, T, P, a, a_0, m, beta, k_B, delta_t, fp);
+    // char filename[50];
+    // sprintf(filename, "data/task_3/data_dt_%.3f_%i_%i.csv", delta_t, its, its_eq);
+    // FILE *fp = fopen(filename, "w");
+    // fprintf(fp, "its, t_max, delta_t, its_eq\n%i, %i, %f, %i\n", its, t_max, delta_t, its_eq);
+    // fprintf(fp, "E_kin, E_pot, E_tot, <T>, T, P, a\n");
+    
+    // verlet(positions, velocities, forces, its, its_eq, delta_t, m, k_B, beta, a_0, N, fp, T_eq, P_eq);
 
     fclose(fp);
+    gsl_rng_free(r);
     destroy_2D_array(positions);
+    destroy_2D_array(forces);
     destroy_2D_array(velocities);
-    free(T);
-    free(P);
 
     // ********************************* Task 4 ********************************** //
     
@@ -162,79 +137,68 @@ init_gsl_rng(int seed){
 }
 
 void 
-verlet(double **positions, double **velocities, double **forces, int its, 
-            double a, double delta_t, double m, double k_B, int N, FILE *fp)
+verlet(double **positions, double **velocities, double **forces, int its, int its_eq, 
+            double delta_t, double m, double k_B, double beta, double a_0, int N, 
+            FILE *fp, double T_eq, double P_eq)
 {   
+    double tau_T = delta_t * 100;
+    double tau_P = delta_t * 500;
+    double alpha_T;
+    double alpha_P = 1.;
     double E_pot = 0.0;
-    calculate(&E_pot, NULL, forces, positions, a * N, N);
+    double virial = 0.0;
+    double P;
+    double *T = malloc(its * sizeof(double));
     double T_avg = 0.0;
-    double *E_kin_tot = malloc(its * sizeof(double));
+    calculate(&E_pot, &virial, forces, positions, a_0 * N, N);
+
     for (unsigned int i = 0; i < its; i++)
     {   
         double E_kin = 0.0;
         // Perform the first half step
         for (unsigned int j = 0; j < N; j++)
         {
-            double *vels = velocities[j];
-            E_kin += 0.5 * m * vector_norm(vels, 3) * vector_norm(vels, 3);
+            E_kin += 0.5 * m * vector_norm(velocities[j], 3) * vector_norm(velocities[j], 3);
             for (unsigned int k = 0; k < 3; k++)
             {
-                vels[k] += 0.5 * delta_t * forces[j][k] / m;
-                positions[j][k] += delta_t * vels[k];
+                velocities[j][k] += 0.5 * delta_t * forces[j][k] / m;
+                positions[j][k] += delta_t * velocities[j][k];
             }
         }
-        E_kin_tot[i] = E_kin;
-        T_avg = average(E_kin_tot, i+1) * 2.0 / 3.0 / k_B / N;
-        if (fp != NULL){
+
+        T[i] = 2.0 / 3.0 / k_B / N * E_kin;
+        T_avg = average(T, i+1);
+        if (i < its_eq){
+            a_0 = a_0 * pow(alpha_P, 1. / 3.);
+            T[0] = 0.1;
+            P = 1 / (3 * 64 * pow(a_0, 3) * alpha_P) * (E_kin + virial);
+
+            alpha_T = 1 + 2 * delta_t * (T_eq - T[i]) / (T[i] * tau_T);
+            alpha_P = 1 - beta * delta_t * (P_eq - P) / tau_P;
+        }
+
+        if (fp != NULL && i >= its_eq){
             fprintf(fp, "%f, %f, %f, %f\n", E_kin, E_pot, E_kin + E_pot, T_avg);
+        }
+        else if (fp != NULL && i < its_eq){
+            fprintf(fp, "%f, %f, %f, %f, %f, %f, %f\n", E_kin, E_pot, E_kin + E_pot, T_avg, T[i], P, a_0);
         }
 
         // Calculate new accelerations
-        calculate(&E_pot, NULL, forces, positions, a * N, N);
+        calculate(&E_pot, &virial, forces, positions, a_0 * N, N);
 
         // Perform the second half step
         for (unsigned int i = 0; i < N; i++)
         {
-            double *vels = velocities[i];
             for (unsigned int j = 0; j < 3; j++)
             {
-                vels[j] += 0.5 * delta_t * forces[i][j] / m;
+                velocities[i][j] += 0.5 * delta_t * forces[i][j] / m;
+            }
+            if (i < its_eq){
+                multiplication_with_constant(positions[i], positions[i], pow(alpha_P, 1. / 3.), 3);
+                multiplication_with_constant(velocities[i], velocities[i], sqrt(alpha_T), 3);
             }
         }
     }
-}
-
-void
-equilibrium(int its, int N, double T_eq, double P_eq, 
-            double **positions, double **velocities, double *T, double *P, 
-            double *a, double a_0, double m, double beta, double k_B, double delta_t, FILE *fp)
-{
-    double tau_T = delta_t * 100; double tau_P = delta_t * 500;
-    double alpha_T;
-    double alpha_P = 1.;
-    a[0] = a_0;
-    for (unsigned int i = 0; i < its; i++)
-    {   
-        double E_k = 0.;
-        for (unsigned int j = 0; j < N; j++)
-        {
-            E_k += 0.5 * m * vector_norm(velocities[j], 3) * vector_norm(velocities[j], 3);
-        }
-        T[i] = 2.0 / 3.0 / k_B / N * E_k;
-        P[i] = 1 / (3 * 64 * pow(a_0, 3) * alpha_P) * (E_k + get_virial_AL(positions, a[i] * N, N));
-
-        alpha_T = 1 + 2 * delta_t * (T_eq - T[i]) / (T[i] * tau_T);
-        alpha_P = 1 - beta * delta_t * (P_eq - P[i]) / tau_P;
-        fprintf(fp, "%f, %f, %f, %f, %f\n", T[i], P[i], a[i], alpha_T, alpha_P);
-
-        if (i < its - 1){
-            a[i+1] = a[i] * pow(alpha_P, 1. / 3.);
-        }
-
-        for (unsigned int j = 0; j < N; j++)
-        {
-            multiplication_with_constant(velocities[j], velocities[j], sqrt(alpha_T), 3);
-            multiplication_with_constant(positions[j], positions[j], pow(alpha_P, 1. / 3.), 3);
-        }
-    }
+    free(T);
 }
