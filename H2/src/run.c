d286c8bb02064a8fa27bf4f8cc63b2c5584951e1
Oscar Stommendal <oscar.stommendal01@gmail.com>
2024-12-01 16:11:01 +0100

    simulating long cooks

--- H2/src/run.c
+++ H2/src/run.c
@@ -2,7 +2,7 @@
 #include <math.h>
 #include <stdlib.h>
 #include "tools.h"
-#include "run.h"
+//#include "run.h"
 #include <gsl/gsl_rng.h>
 #include <gsl/gsl_randist.h>
 
@@ -20,45 +20,38 @@ run(
     double E_znzn = -0.113; // eV
     double E_cuzn = -0.294; // eV
     double k_B = 8.617333262145e-5; // eV/K
+    double N = 2500000;
 
-    double *T = (double *)malloc(500 * sizeof(double));
-    for (int i = 0; i < 500; i++)
-    {
-        T[i] = i * 2;
-    }
     FILE *file = fopen("P_T.csv", "w");
     double delta_E = E_cucu + E_znzn - 2 * E_cuzn;
-    for (int i = 0; i < 500; i++)
+    double *T = (double *)malloc(N * sizeof(double));
+    double *P = (double *)malloc(N * sizeof(double));
+    P[0] = -1.;
+    double step_size = 0.00000001;
+    for (int i = 1; i < N-1; i++)
     {   
-        double P = 0.5;
-        newton_raphson(&P, T[i], delta_E, k_B, file);
+        P[i] = P[i-1] + step_size;
+        if (fabs(P[i]) > 0.99)
+        {   
+            step_size = 0.00000001;
+        }
+        else if (fabs(P[i]) <= 0.99 )
+        {
+            step_size = 0.001;
+        }
+        else if (fabs(P[i]) > 1)
+        {
+            break;
+        }
+        T[i] = 4 * P[i] * delta_E / k_B / log((1 + P[i]) / (1 - P[i]));
+        fprintf(file, "%f, %f, %f\n", P[i], T[i], step_size);
     }
 
     fclose(file);
     free(T);
+    free(P);
 
     return 0;
-}
-
-double PT_fun(double P, double T, double delta_E, double k_B)
-{
-    return -4. * P * delta_E + k_B * T *(log(1 + P) - log(1 - P));
-}
-
-void newton_raphson(double *P, double T, double delta_E, double k_B, FILE *file)
-{
-    double tol = 1e-6;
-    double f = PT_fun(*P, T, delta_E, k_B);
-    double df = -4. * delta_E + k_B * T * (1 / (1 + *P) + 1 / (1 - *P));
-    double delta_P = f / df;
-    while (fabs(delta_P) > tol)
-    {
-        *P -= delta_P;
-        f = PT_fun(*P, T, delta_E, k_B);
-        df = -4. * delta_E + k_B * T * (1 / (1 + *P) + 1 / (1 - *P));
-        delta_P = f / df;
-    }
-    fprintf(file, "%f, %f\n", T, *P);
 }
 
 gsl_rng *
