6dfba693948d4129d0a026d02e21e26d2661cb3f
j-haralds <jonatan.haraldsson@icloud.com>
2024-12-01 13:47:18 +0100

    gsl_init

--- H2/src/run.c
+++ H2/src/run.c
@@ -1,6 +1,10 @@
 #include <stdio.h>
 #include <math.h>
 #include <stdlib.h>
+#include "tools.h"
+#include "run.h"
+#include <gsl/gsl_rng.h>
+#include <gsl/gsl_randist.h>
 
 
 int
@@ -13,4 +17,24 @@ run(
     // This makes it possible to test
     // 100% of you code
     return 0;
+}
+
+
+gsl_rng *
+init_gsl_rng(int seed){
+    const gsl_rng_type * T;
+    gsl_rng * r;
+    gsl_rng_env_setup();
+    T = gsl_rng_default; // default random number generator
+    r = gsl_rng_alloc(T); // allocate memory for the random number generator
+
+    if (!r) {
+        fprintf(stderr, "Error: Could not allocate memory for RNG.\n");
+        exit(EXIT_FAILURE); // Exit if allocation fails
+    }
+
+    // Set the seed
+    gsl_rng_set(r, seed);
+
+    return r;
 }
