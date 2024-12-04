30d9c6ebbe6bc8cc1ba9b34c4ca6f0fd98cb095b
Oscar Stommendal <oscar.stommendal01@gmail.com>
2024-12-04 13:11:00 +0100

    smart flash cook

--- H2/src/lattice.c
+++ H2/src/lattice.c
@@ -1,7 +1,6 @@
 #include <stdio.h>
 
-void init_sc(double **positions, int N, double lattice_param, double base[3], 
-             int **neighbors, int N_neighbors, FILE *f_neighbors)
+void init_sc(double **positions, int N, double lattice_param, double base[3])
 {   
     int ind = 0;
     for (int x = 0; x < N; x++)
@@ -16,22 +15,6 @@ void init_sc(double **positions, int N, double lattice_param, double base[3],
                 ind += 1;
 		    }
         }
-    }
-    double help_plz[8] = {0, 1, 10, 11, 100, 101, 110, 111};
-    for (int j = 0; j < N_neighbors; j++)
-    {
-        for (int i = 0; i < N*N*N; i++)
-        {
-            neighbors[i][j] = (int)(help_plz[j] + i);
-        } 
-    }
-    for (int i = 0; i < N*N*N; i++)
-    {
-        for (int j = 0; j < N_neighbors; j++)
-        {
-            fprintf(f_neighbors, "%i, ", neighbors[i][j]);
-        }
-        fprintf(f_neighbors, "\n");
     }
 }
 
--- H2/src/run.c
+++ H2/src/run.c
@@ -6,6 +6,12 @@
 #include <gsl/gsl_rng.h>
 #include <gsl/gsl_randist.h>
 
+gsl_rng * init_gsl_rng(int seed);
+double boundary_distance_between_vectors(double *v1, double *v2, int dim, double box_length);
+void nearest_neighbors_bcc(double **pos_A, double **pos_B, int N_A, int N_B, 
+                            double box_length, double cutoff, double closest_distance, 
+                            FILE *fp_A, FILE *fp_B);
+
 int
 run(
     int argc,
@@ -19,6 +25,7 @@ run(
     // double k_B = 8.617333262145e-5; // eV/K
     // double N = 2500000;
     double a = 2.949; // [Å]
+    double closest_distance_bcc = sqrt(3) * a / 2;
 
     // FILE *file = fopen("P_T.csv", "w");
     // double delta_E = E_cucu + E_znzn - 2 * E_cuzn;
@@ -49,16 +56,35 @@ run(
     // free(T);
     // free(P);
 
-    double **sub_A = create_2D_array(1000, 3);
-    // double **sub_B = create_2D_array(1000, 3);
-    int **neighbors = (int **)create_2D_array(1000,8);
+    int L = 10;
+    int N_atoms = 2 * L * L * L;
+    double **sub_A = create_2D_array(N_atoms / 2, 3);
+    double **sub_B = create_2D_array(N_atoms / 2, 3);
 
     char filename[100];
-    sprintf(filename, "neighborsB.csv");
-    FILE *fn_B = fopen(filename, "w");
-    init_sc(sub_A, 10, a, (double[3]){0, 0, 0}, neighbors,8, fn_B);
-    fclose(fn_B);
-    //init_sc(sub_B, 10, a, (double[1][3]){{0.5, 0.5, 0.5}});
+    sprintf(filename, "data/task_2/neighborsA.csv");
+    FILE *fp_NN_A = fopen(filename, "w");
+    sprintf(filename, "data/task_2/neighborsB.csv");
+    FILE *fp_NN_B = fopen(filename, "w");
+
+    init_sc(sub_A, L, a, (double[3]){0, 0, 0});
+    init_sc(sub_B, L, a, (double[3]){0.5, 0.5, 0.5});
+    // format filename with variable L
+    sprintf(filename, "data/task_2/sub_A_%i.csv", L);
+    FILE *fp_A = fopen(filename, "w");
+    sprintf(filename, "data/task_2/sub_B_%i.csv", L);
+    FILE *fp_B = fopen(filename, "w");
+    for (int i = 0; i < N_atoms / 2; i++)
+    {
+        fprintf(fp_A, "%f, %f, %f\n", sub_A[i][0], sub_A[i][1], sub_A[i][2]);
+        fprintf(fp_B, "%f, %f, %f\n", sub_B[i][0], sub_B[i][1], sub_B[i][2]);
+    }
+
+    nearest_neighbors_bcc(sub_A, sub_B, 1000, 1000, 10 * a, 0.1, closest_distance_bcc, fp_NN_A, fp_NN_B);
+    fclose(fp_NN_A);
+    fclose(fp_NN_B);
+    destroy_2D_array(sub_A);
+    destroy_2D_array(sub_B);
 
     return 0;
 }
@@ -82,5 +108,67 @@ init_gsl_rng(int seed){
     return r;
 }
 
+double
+boundary_distance_between_vectors(double *v1, double *v2, int dim, double box_length)
+{
+    double r = 0.0;
+    double delta;
+    for (int d = 0; d < dim; d++) 
+    {
+        delta = v1[d] - v2[d];
+        // Apply boundary conditions
+        delta -= round(delta / box_length) * box_length;
+        r += delta * delta;
+    }
 
+    return sqrt(r);
+}
 
+void
+nearest_neighbors_bcc(double **pos_A, double **pos_B, int N_A, int N_B, 
+                        double box_length, double cutoff, double closest_distance, 
+                        FILE *fp_A, FILE *fp_B)
+{
+    double r;
+    for (int i = 0; i < N_A; i++)
+    {   
+        int count = 0;
+        for (int j = 0; j < N_B; j++)
+        {
+            r = boundary_distance_between_vectors(pos_A[i], pos_B[j], 3, box_length);
+            if (fabs(closest_distance - r) < cutoff)
+            {   
+                count++;
+                if (count < 8)
+                {
+                    fprintf(fp_A, "%i, ", j);
+                }
+                else if (count == 8)
+                {
+                    fprintf(fp_A, "%i\n", j);
+                }
+            }
+        }
+    }
+    for (int i = 0; i < N_B; i++)
+    {   
+        int count = 0;
+        for (int j = 0; j < N_A; j++)
+        {
+            r = boundary_distance_between_vectors(pos_B[i], pos_A[j], 3, box_length);
+            if (fabs(closest_distance - r) < cutoff)
+            {
+                count++;
+                if (count < 8)
+                {
+                    fprintf(fp_B, "%i, ", j);
+                }
+                else if (count == 8)
+                {
+                    fprintf(fp_B, "%i\n", j);
+                }
+            }
+        }
+        fprintf(fp_B, "\n");
+    }
+}
